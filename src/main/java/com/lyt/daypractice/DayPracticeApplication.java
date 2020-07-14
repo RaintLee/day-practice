54156b49aa4bf9a73588044448c0094f82a90f6b
liyutian <liyutian@vipkid.com.cn>
2020-07-14 10:37:28 +0800

    初始化

---
+++ src/main/java/com/lyt/daypractice/DayPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.lyt.daypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyt/daypractice/DayPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyt.daypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
